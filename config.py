90e4be9811b51f2e351a88fb68ad096d6b7eab7b
Titonop <02sha.rmayt@gmail.com>
2024-12-05 10:48:58 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,15 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY @MrRanDom8
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7788865701:AAEHVM8BGHCY_fIECUSn_ydJ_45Jn0p72-M")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("Titanop24")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("7163028849")
+
+
+
+  
